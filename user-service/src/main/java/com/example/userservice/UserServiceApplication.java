2d50746b9986aaff29f65d0a74a5974878f227db
hanul <hanultchr@gmail.com>
2025-08-29 15:19:20 +0900

    Initial commit

---
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -0,0 +1,40 @@
+package com.example.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@SpringBootApplication
+@EnableRedisHttpSession
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        int TIMEOUT = 5000;
+
+        RestTemplate restTemplate = new RestTemplateBuilder()
+                .setConnectTimeout(Duration.ofMillis(TIMEOUT))
+                .setReadTimeout(Duration.ofMillis(TIMEOUT))
+                .build();
+
+        return restTemplate;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+}
