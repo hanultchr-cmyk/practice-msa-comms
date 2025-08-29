2c7462f558d58223b7434d70d277aac3a33f339f
hanul <hanultchr@gmail.com>
2025-08-29 15:16:36 +0900

    Initial commit

--- apigateway-service/src/main/java/com/example/apigatewayservice/controller/BffController.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/controller/BffController.java
@@ -1,7 +1,5 @@
 package com.example.apigatewayservice.controller;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -11,7 +9,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/bff")
-@Slf4j
 public class BffController {
 
     private final WebClient.Builder loadBalancedWebClientBuilder;
@@ -23,7 +20,6 @@ public class BffController {
     // 회원 가입 (User-Service 호출)
     @PostMapping("/signup")
     public Mono<String> signup(@RequestBody Map<String, Object> user) {
-        log.info("signup method called");
         return loadBalancedWebClientBuilder.build()
                 .post()
                 .uri("http://user-service/users")
@@ -34,29 +30,18 @@ public class BffController {
 
     // 로그인 (User-Service 호출)
     @PostMapping("/login")
-    public Mono<ResponseEntity<String>> login(@RequestBody Map<String, Object> credentials) {
-        log.info("login method called");
+    public Mono<String> login(@RequestBody Map<String, Object> credentials) {
         return loadBalancedWebClientBuilder.build()
                 .post()
                 .uri("http://user-service/login")
                 .bodyValue(credentials)
                 .retrieve()
-                .toEntity(String.class)
-                .map(clientResponse -> {
-                    String body = clientResponse.getBody();
-                    String customHeader = clientResponse.getHeaders().getFirst("Token");
-
-                    return ResponseEntity
-                            .ok()
-                            .header("Token", customHeader != null ? customHeader : "N/A")
-                            .body(body);
-                });
+                .bodyToMono(String.class);
     }
 
     // 전체 회원 목록 조회 (User-Service 호출)
     @GetMapping("/users")
     public Mono<List<Object>> getAllUsers() {
-        log.info("getAllUsers method called");
         return loadBalancedWebClientBuilder.build()
                 .get()
                 .uri("http://user-service/users")
@@ -66,10 +51,26 @@ public class BffController {
     }
 
     // 회원 상세 조회 (User-Service 호출)
-
+    @GetMapping("/users/{userId}")
+    public Mono<List<Object>> getUser(@PathVariable String userId) {
+        return loadBalancedWebClientBuilder.build()
+                .get()
+                .uri("http://user-service/users/{userId}", userId)
+                .retrieve()
+                .bodyToFlux(Object.class)
+                .collectList();
+    }
 
     // 주문하기 (Order-Service 호출)
-
+    @PostMapping("/orders/{userId}")
+    public Mono<String> createOrder(@PathVariable String userId, @RequestBody Map<String, Object> orderRequest) {
+        return loadBalancedWebClientBuilder.build()
+                .post()
+                .uri("http://order-service/{userId}/orders", userId)
+                .bodyValue(orderRequest)
+                .retrieve()
+                .bodyToMono(String.class);
+    }
 
     // 주문 목록 확인 (Order-Service 호출)
     @GetMapping("/orders/{userId}")
